f4b5c17a219b79c250fc7a7b5f243a8bbe7b9cd6
Brock13s <48967600+Brock13s@users.noreply.github.com>
2023-05-11 18:37:06 +1000

    Add files via upload

--- tempappend.py
+++ tempappend.py
@@ -17,34 +17,40 @@ url = input("Enter IP: ")
 url = f"http://{url}/temp-graph"
 interval = input("Enter the time interval (in seconds, default is 60): ")
 interval = int(interval) if interval.isdigit() else 60
-file_path = "response_log.txt"
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "response_log.csv")
 
 time.sleep(2)
 with open(file_path, "w") as file:
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(current_time, " Clearing previous data in response_log file")
-    file.write("Cleared Previous Data. New log data incoming....\n\n")
+    print("Clearing previous data in response_log file")
+    file.write("Time,Temperature\n\n")
+    
 time.sleep(2)
 
 print("Script started! Fetching content from {}...".format(url))
 time.sleep(2)
 counter = 0
-while True: 
-    content = get_response_content(url)
+try:
+    while True: 
+        content = get_response_content(url)
 
-    if content:
-        counter +=1
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        log_entry = "{} - {}\n".format(current_time, content)
+        if content:
+            counter += 1
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+            log_entry = "{},{}\n".format(current_time, content)
 
-        with open(file_path, "a") as file:
-            file.write(log_entry)
+            with open(file_path, "a") as file:
+               file.write(log_entry)
 
-        if os.path.isfile(file_path) and os.stat(file_path).st_size > 0:
-            print(f"|{counter}| Data appended successfully.")
+            if os.path.isfile(file_path) and os.stat(file_path).st_size > 0:
+                print(f"|{counter}| Data appended successfully.")
+            else:
+                print("Error: Failed to append data to file.")
         else:
-            print(f"|{counter}| Error: Failed to append data to file.")
-    else:
-        print("Error: Failed to retrieve content from the URL.")
+            print("Error: Failed to retrieve content from the URL.")
 
-    time.sleep(interval)
+        time.sleep(interval)
+
+except KeyboardInterrupt:
+    print("Keyboard interrupt detected. Closing the file...")
+    # Close the file before exiting
+    file.close()
